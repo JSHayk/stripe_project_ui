c55b1f6bde35183cf1a1964ccf4a3a63f4fcb800
JSHayk <jshaykdeveloper@gmail.com>
2022-11-27 21:29:07 +0400

    changed base_url in

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8080/api";
+export const BASE_URL = "https://stripe-project-api.onrender.com/api";
 
 const $api = axios.create({
   baseURL: BASE_URL,
